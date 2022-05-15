508c7e65cf82aaee651654a145d76f81920aeadd
EC2 Default User <ec2-user@ip-172-31-42-57.ec2.internal>
2022-05-15 07:55:26 +0000

    Finish bookers-debug2 follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,37 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+
+  # フォロワー機能
+  # ーーーーーーーーーーーーーーーーーーーー
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # ーーーーーーーーーーーーーーーーーーーー
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
+  # フォロワー機能メソッド
+  # ーーーーーーーーーーーーーーーーーーーー
+  # フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # フォローを外すときの処理
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしているか判定
+  def following?(user)
+    followings.include?(user)
+  end  
+  
+  # ーーーーーーーーーーーーーーーーーーーー
 
   def get_profile_image
     unless profile_image.attached?
