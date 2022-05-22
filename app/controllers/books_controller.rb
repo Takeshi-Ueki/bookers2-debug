62dad17f2336787f4e487334ff05a6562aaed650
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-05-22 02:02:46 +0000

    Finish7a week-ranking

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,14 @@ class BooksController < ApplicationController
   end
 
   def index
-    @books = Book.all
+    to = Time.current.end_of_day
+    from = (to - 6.day).beginning_of_day
+    @books = Book.all.sort { |a,b|
+      b.favorites.where(created_at: from..to).size <=>
+      a.favorites.where(created_at: from..to).size
+    }
     @book = Book.new
+
   end
 
   def create
