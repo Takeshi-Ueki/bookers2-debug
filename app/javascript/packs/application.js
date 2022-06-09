91412a77825e2c42a95ef2fd2025f24e0f36c8dd
Takeshi-Ueki <takeshi.c.0104@gmail.com>
2022-06-09 13:37:59 +0000

    chart.js cdn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+window.$ = window.jQuery = require('jquery');
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,8 +14,6 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
-
-window.$ = window.jQuery = require('jquery');
 
 require("chartkick")
 require("chart.js")
